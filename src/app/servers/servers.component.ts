900fa77d1b2d6f15d9071471ebe352f5ee9c51bd
Thibetanus <steventhibe@gmail.com>
2023-03-05 23:05:44 +0800

    finish assignment1: Practicing Components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
